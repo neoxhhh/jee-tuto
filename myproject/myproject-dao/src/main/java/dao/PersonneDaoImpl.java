773599d89404e65f829ce0e0e5e6e058559dc622
ahmed <ahmed@izy.Home>
2016-10-18 11:13:56 +0100

    commit added class in DAO Layer

---
+++ myproject/myproject-dao/src/main/java/dao/PersonneDaoImpl.java
@@ -0,0 +1,21 @@
+package dao;
+
+import entities.Personne;
+
+public class PersonneDaoImpl implements IPersonneDao {
+
+	public PersonneDaoImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void addPersonneDao(Personne personne) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Personne getPersonneByIdDao(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
